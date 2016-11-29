258f2166f6ca005ca47b927cad107e9928b04e93
codeliu6572 <codeliu6572@163.com>
2016-11-29 15:59:06 +0800

    Initial Commit

---
+++ Https/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Https
+//
+//  Created by 刘浩浩 on 2016/11/29.
+//  Copyright © 2016年 CodingFire. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ HttpsTests/HttpsTests.m
@@ -0,0 +1,39 @@
+//
+//  HttpsTests.m
+//  HttpsTests
+//
+//  Created by 刘浩浩 on 2016/11/29.
+//  Copyright © 2016年 CodingFire. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HttpsTests : XCTestCase
+
+@end
+
+@implementation HttpsTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
